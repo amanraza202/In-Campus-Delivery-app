a7420fc4a9848665c3f826a5045bddeba4c8214a
Aman Raza <80668893+amanraza202@users.noreply.github.com>
2023-07-11 22:38:28 +0530

    First Commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import Aman_Gupta from "./Assets/1.jpg";
+import Aman from "./Assets/3.jpeg";
+import Tanay from "./Assets/2.jpg";
+
+
+export const imagesData = [
+  {
+    id: 1,
+    name: "Aman Gupta",
+    uid: "20BCE10347",
+    image: Aman_Gupta,
+  },
+  {
+    id: 2,
+    name: "Aman Raza",
+    uid: "20BCE10348",
+    image: Aman,
+  },
+  {
+    id: 3,
+    name: "Tanay Dwivedi",
+    uid: "20BCE10367",
+    image: Tanay,
+  },
+ 
+
+];
